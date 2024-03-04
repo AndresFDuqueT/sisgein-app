3ac1c03ab8b8fa0f8d43aa8a1657733c5d05caee
Andres Felipe Duque <afduque32@soy.sena.edu.co>
2024-03-03 20:35:24 -0500

    organizar el contenido del modal de la pag inventario

--- src/Pages/Inventario/Inventario.jsx
+++ src/Pages/Inventario/Inventario.jsx
@@ -76,18 +76,86 @@ const Inventario = () => {
             Agregar Item
           </button>
         </div>
-
         <DataTableInv data={data} />
       </div>
       <Modal open={open} onClose={() => setOpen(false)}>
-        <div className="text-center w-56">
-          <div className="mx-auto my-4 w-48">
-            <h3 className="text-lg font-black text-gray-800">Confirm Delete</h3>
-            <p className="text-sm text-gray-500">
-              Are you sure you want to delete this item?
-            </p>
+        <div className=" w-2/4">
+          <div className="mx-auto my-4 w-96">
+            <h3 className="text-lg text-center font-black text-gray-800">
+              Creacion de Nuevo Item
+            </h3>
+            <div className="flex flex-row gap-1 justify-center ">
+              <h5 className="text-lg font-semibold">Almacen:</h5>
+              <h6 className="text-lg font-medium"> La Principal</h6>
+            </div>
+            <div className="flex flex-row gap-1 justify-center ">
+              <h5 className="text-lg font-semibold">Item ID:</h5>
+              <h6 className="text-lg font-medium"> 10153</h6>
+            </div>
+
+            {/* Form Basica para la creaion de un item nuevo */}
+            <form className="max-w-md mx-auto p-4 bg-white rounded-md shadow-md">
+              <label className="block mb-2">
+                Nombre del Producto:
+                <input
+                  type="text"
+                  name="productName"
+                  placeholder="Por favor introducir el nombre del producto"
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Cantidad:
+                <input
+                  type="number"
+                  name="quantity"
+                  placeholder="0"
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Editorial:
+                <input
+                  type="text"
+                  name="editorial"
+                  placeholder="Por favor introducir el nombre de la editorial"
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Proveedor:
+                <input
+                  type="text"
+                  name="provider"
+                  placeholder="Por favor introducir el nombre del proveedor "
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Géneros:
+                <input
+                  type="text"
+                  name="genres"
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+
+              <label className="block mb-2">
+                Idioma:
+                <input
+                  type="text"
+                  name="language"
+                  placeholder="Por favor intrucir los idiomas del libro"
+                  className="w-full px-2 py-1 border rounded-md"
+                />
+              </label>
+            </form>
           </div>
-          <div className="flex gap-4">
+          <div className="flex justify-end gap-4">
             <button
               className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded w-full"
               onClick={() => setOpen(false)}
